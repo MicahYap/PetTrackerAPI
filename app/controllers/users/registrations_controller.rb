08ec50b21fd269bbd6bd685c1ae6704af0266bf9
micahyap <yapmicahsy@gmail.com>
2024-09-04 16:18:12 +0800

    added create method in registrations controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,7 +3,20 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   respond_to :json
 
+  def create
+    user=User.new(sign_up_params)
+    if user.save
+      render json: { message: 'User created successfully', jwt: encode_token(user) }, status: :ok
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def sign_up_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 
   def respond_with(resource, _opts = {})
     if resource.persisted?
