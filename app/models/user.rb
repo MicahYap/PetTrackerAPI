191d8b81b247d6846999710e7251aef29562a0d0
micahyap <yapmicahsy@gmail.com>
2024-10-02 09:16:19 +0800

    added min password character

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,16 @@ class User < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :password, length: { minimum: 6, message: 'must be more than 6 characters' }, if: :password_required?
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
 
   has_many :pets, dependent: :destroy
+
+  private
+
+  def password_required?
+    !persisted? || !password.nil? || !password_confirmation.nil?
+  end
 end
